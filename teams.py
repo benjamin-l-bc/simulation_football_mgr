568189094bda0c6cdf9c69d0bb55d85aec545450
benjaminshi02003220 <346473509@qq.com>
2018-04-19 00:45:31 +0800

    team

---
+++ teams.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 00:11:17 2018
+
+@author: v-beshi
+"""
+import new_player
+manu={'players':[],'Finance':0,'savings':0} 
+real={'players':[],'Finance':0,'savings':0} 
+psg={'players':[],'Finance':0,'savings':0} 
+ac={'players':[],'Finance':0,'savings':0} 
+new_player=new_player.create_new(10)
+print(new_player)
+
+for i in range(10):
+    
+
+    team['players'].append(new_player.loc(num))
+    new_player=new_player.drop([num])
+    
+    
+    
+    
